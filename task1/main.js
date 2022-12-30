d334f976273ca1b0b84cb4e01507d790e4e4e07e
ryckabass0827 <laiskasrichardui@gmail.com>
2022-12-30 14:22:35 +0200

    first commit

---
+++ task1/main.js
@@ -0,0 +1,4 @@
+/*
+  Parašyti JS kodą, kuris leis vartotojui įvesti temperatūrą celsijumi arba farenheitu.
+  Įvedus celsijų - parodoma kiek temperatūros tai būtų farenheitu | įvedus farenheitą - parodoma kiek temperatūros tai būtų celsijumi.
+*/
